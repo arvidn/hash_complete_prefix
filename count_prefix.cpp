22ece25f1558ccdccae4712ef56c187b72942800
arvidn <arvid@rasterbar.com>
2014-01-04 19:53:32 -0800

    filter out IP addresses that are not allowed or not in use on the global internet. Apply the IP masking that's proposed in the dht security document (and update it to preserve a bit more entropy). add the python script that plots the graph of number of bits of input values are controlled by the number of bits in the IPs you control. add a test for uniform distribution of sha-1 and crc32

--- count_prefix.cpp
+++ count_prefix.cpp
@@ -6,91 +6,183 @@
 #include "trie.hpp"
 #include "MurmurHash3.h"
 
-uint32_t murmur3(uint32_t input)
+const int num_buckets = 1000;
+
+uint32_t murmur3(uint32_t input, char r)
 {
 	uint32_t ret;
-	MurmurHash3_x86_32(&input, 4, 0, &ret);
+	char buf[5];
+	memcpy(buf, &input, 4);
+	buf[4] = r;
+	MurmurHash3_x86_32(buf, 5, 0, &ret);
 	return ret;
 }
 
-uint32_t sha(uint32_t input)
+uint32_t sha(uint32_t input, char r)
 {
 	SHA_CTX ctx;
 	SHA_Init(&ctx);
 	SHA_Update(&ctx, &input, 4);
+	SHA_Update(&ctx, &r, 1);
 	uint32_t digest[5];
 	SHA_Final((unsigned char*)digest, &ctx);
 	return digest[0];
 }
 
-uint32_t sha_xor(uint32_t input)
-{
-	SHA_CTX ctx;
-	SHA_Init(&ctx);
-	SHA_Update(&ctx, &input, 4);
-	uint32_t digest[5];
-	SHA_Final((unsigned char*)digest, &ctx);
-	return digest[0] ^ digest[1] ^ digest[2] ^ digest[3] ^ digest[4];
-}
-
-uint32_t crc(uint32_t input)
+uint32_t crc(uint32_t input, char r)
 {
 	uint32_t out = crc32(0L, Z_NULL, 0);
-	return crc32(out, (const Bytef*)&input, 4);
+	out = crc32(out, (const Bytef*)&input, 4);
+	return crc32(out, (const Bytef*)&r, 1);
 }
 
-uint32_t adler(uint32_t input)
+uint32_t adler(uint32_t input, char r)
 {
 	uint32_t out = adler32(0L, Z_NULL, 0);
-	return adler32(out, (const Bytef*)&input, 4);
+	out = adler32(out, (const Bytef*)&input, 4);
+	return adler32(out, (const Bytef*)&r, 1);
 }
 
-template <class F>
-void for_each_input(F fun, char const* filename)
+template <class F, class P>
+void for_each_input(F fun, P predicate, char const* filename)
 {
 	// the collection of all output values, as
 	// a dense set representation
 	trie out_values;
 
 	FILE* fout = fopen(filename, "w+");
-
-	uint32_t output = fun(0);
-	out_values.insert(output);
-
-	for (int bits = 0; bits < 32; ++bits)
+	const uint64_t max = 0x100000000LL;
+	uint64_t next_print = 1;
+	uint64_t num_ips = 0;
+	for (uint64_t i = 0; i < max; ++i)
 	{
-		// the way we produce the increasing number of input "entropy"
-		// is by first setting the first bit, then for every combination
-		// of bit prefixes, set the next bit
-		// round 1:
-		// 10000000 00000000 00000000 00000000
+		if (!predicate(i)) continue;
 
-		// round 2:
-		// 01000000 00000000 00000000 00000000
-		// 11000000 00000000 00000000 00000000
+		// also, only include the values where the filtered-out
+		// bits are zero. All values where these bits aren't
+		// zero will be masked into the versions with zeros
+		// anyway, and we might as well only run the hash function
+		// once, instead of for each duplicate.
+//		if ((i & ~0x01071f7f) != 0) continue;
+		if ((i & ~0x030f3fff) != 0) continue;
 
-		// round 3:
-		// 00100000 00000000 00000000 00000000
-		// 01100000 00000000 00000000 00000000
-		// 10100000 00000000 00000000 00000000
-		// 11100000 00000000 00000000 00000000
-		// and so on. For each round we have a complete
-		// bit prefix of 'bits' in the input. We then count
-		// the complete bit prefix of the SHA-1 digest
-
-		const uint32_t max = 1 << bits;
-		for (uint32_t i = 0; i < max; ++i)
+		uint32_t input = htonl(i);
+		for (int r = 0; r < 8; ++r)
 		{
-			uint32_t input = (i << (32 - bits)) | (1 << (31 - bits));
-			uint32_t output = fun(input);
+			uint32_t output = fun(input, r);
 			out_values.insert(output);
-		}
 
-		int in_bits = bits + 1;
-		int complete_bit_prefix = out_values.count_complete_prefix();
-		fprintf(fout, "%d\t%d\n", in_bits, complete_bit_prefix);
-		fflush(fout);
+			++num_ips;
+			if (num_ips == next_print) {
+				int complete_bit_prefix = out_values.count_complete_prefix();
+				fprintf(fout, "%lld\t%d\n", num_ips, complete_bit_prefix);
+				next_print <<= 1;
+			}
+		}
 	}
+
+	int complete_bit_prefix = out_values.count_complete_prefix();
+	fprintf(fout, "%lld\t%d\n", num_ips, complete_bit_prefix);
+
+	fclose(fout);
+}
+
+bool mask(uint32_t in)
+{
+	// don't include IP addresses that are invalid on
+	// the internet
+	if ((in & 0xff000000) == 0x0a000000 // 10.x.x.x
+		|| (in & 0xfff00000) == 0xac100000 // 172.16.x.x
+		|| (in & 0xffff0000) == 0xc0a80000 // 192.168.x.x
+		|| (in & 0xffff0000) == 0xa9fe0000 // 169.254.x.x
+		|| (in & 0xff000000) == 0x7f000000 // 127.x.x.x
+		|| (in & 0xff000000) == 0 // reserved for special use
+		)
+		return false;
+
+	uint8_t first_octet = in >> 24;
+
+	// reserved for multicast and future use
+	if (first_octet >= 224)
+		return false;
+
+	// some /8 blocks don't appear to be in use. Filter those
+	// out to get a more realistic set of IPs we would see
+	// in the wild.
+
+	if (first_octet == 3
+		|| first_octet == 6
+		|| first_octet == 7
+		|| first_octet == 9
+		|| first_octet == 11
+		|| first_octet == 19
+		|| first_octet == 21
+		|| first_octet == 22
+		|| first_octet == 25
+		|| first_octet == 26
+		|| first_octet == 28
+		|| first_octet == 29
+		|| first_octet == 30
+		|| first_octet == 33
+		|| first_octet == 48
+		|| first_octet == 51
+		|| first_octet == 52
+		|| first_octet == 56
+		|| first_octet == 102
+		|| first_octet == 104)
+		return false;
+
+	return true;
+}
+
+template <class F, class P>
+void chi_square_distribution(F fun, P predicate, uint64_t* buckets
+	, uint64_t start, uint64_t end)
+{
+	const uint64_t max = 0x100000000LL;
+	for (uint64_t i = start; i < end; ++i)
+	{
+		if (!predicate(i)) continue;
+
+		uint32_t input = htonl(i & 0x030f3fff);
+//		uint32_t input = htonl(i & 0x01071f7f);
+
+		for (int r = 0; r < 8; ++r)
+		{
+			uint64_t output = fun(input, r);
+			++buckets[output * num_buckets / 0x100000000LL];
+		}
+	}
+
+}
+
+void print_results(uint64_t* buckets1, uint64_t* buckets2
+	, uint64_t* buckets3, uint64_t* buckets4, char const* filename)
+{
+	uint64_t num_samples = 0;
+
+	FILE* fout = fopen(filename, "w+");
+
+	for (int i = 0; i < num_buckets; ++i)
+	{
+		buckets1[i] += buckets2[i];
+		buckets1[i] += buckets3[i];
+		buckets1[i] += buckets4[i];
+		num_samples += buckets1[i];
+		fprintf(fout, "%d\t%lld\n", i, buckets1[i]);
+	}
+
+	double chi_square = 0.;
+	const double expected = double(num_samples) / double(num_buckets);
+
+	for (int i = 0; i < num_buckets; ++i)
+	{
+		double diff = double(buckets1[i]) - expected;
+		chi_square += diff * diff;
+	}
+
+	printf("%s-chi-square: %f num-samples: %lld expected: %f\n"
+		, filename, chi_square, num_samples, expected);
 
 	fclose(fout);
 }
@@ -106,13 +198,43 @@ int main(int argc, char const* argv[])
 	// parallelism (4 threads). The alternative would essentially cause
 	// everything to serialize around inserting (which would need to be
 	// mutex protected anyway).
-	pool.emplace_back([](){for_each_input(sha, "sha.dat"); } );
-	pool.emplace_back([](){for_each_input(sha_xor, "sha_xor.dat"); } );
-	pool.emplace_back([](){for_each_input(crc, "crc.dat"); } );
-	pool.emplace_back([](){for_each_input(adler, "adler.dat"); } );
-	pool.emplace_back([](){for_each_input(murmur3, "murmur3.dat"); } );
+	pool.emplace_back([](){for_each_input(sha, mask, "sha.dat"); } );
+	pool.emplace_back([](){for_each_input(crc, mask, "crc.dat"); } );
+	pool.emplace_back([](){for_each_input(adler, mask, "adler.dat"); } );
+	pool.emplace_back([](){for_each_input(murmur3, mask, "murmur3.dat"); } );
+
+	uint64_t buckets5[num_buckets];
+	uint64_t buckets6[num_buckets];
+	uint64_t buckets7[num_buckets];
+	uint64_t buckets8[num_buckets];
+	memset(buckets5, 0, sizeof(buckets5));
+	memset(buckets6, 0, sizeof(buckets6));
+	memset(buckets7, 0, sizeof(buckets7));
+	memset(buckets8, 0, sizeof(buckets8));
+
+	pool.emplace_back([&](){chi_square_distribution(sha, mask, buckets5, 0, 0x40000000); } );
+	pool.emplace_back([&](){chi_square_distribution(sha, mask, buckets6, 0x40000000, 0x80000000); } );
+	pool.emplace_back([&](){chi_square_distribution(sha, mask, buckets7, 0x80000000, 0xC0000000); } );
+	pool.emplace_back([&](){chi_square_distribution(sha, mask, buckets8, 0xC0000000, 0x100000000); } );
+
+	uint64_t buckets1[num_buckets];
+	uint64_t buckets2[num_buckets];
+	uint64_t buckets3[num_buckets];
+	uint64_t buckets4[num_buckets];
+	memset(buckets1, 0, sizeof(buckets1));
+	memset(buckets2, 0, sizeof(buckets2));
+	memset(buckets3, 0, sizeof(buckets3));
+	memset(buckets4, 0, sizeof(buckets4));
+
+	pool.emplace_back([&](){chi_square_distribution(crc, mask, buckets1, 0, 0x40000000); } );
+	pool.emplace_back([&](){chi_square_distribution(crc, mask, buckets2, 0x40000000, 0x80000000); } );
+	pool.emplace_back([&](){chi_square_distribution(crc, mask, buckets3, 0x80000000, 0xC0000000); } );
+	pool.emplace_back([&](){chi_square_distribution(crc, mask, buckets4, 0xC0000000, 0x100000000); } );
 
 	for (auto& t : pool)
 		t.join();
+
+	print_results(buckets1, buckets2, buckets3, buckets4, "crc-dist.dat");
+	print_results(buckets5, buckets6, buckets7, buckets8, "sha-dist.dat");
 }
 
--- render.gnuplot
+++ render.gnuplot
@@ -1,13 +1,24 @@
 set term png size 1200,700
 set output "complete_bit_prefixes.png"
-set xrange [0:*]
-set xlabel "input complete bit prefix (bits)"
+set xrange [1:*]
+set logscale x
+set xtics 8
+set xlabel "number of (valid) input values"
 set ylabel "output complete bit prefix (bits)"
 set style data lines
 set key box
 plot "sha.dat" using 1:2 title "SHA-1", \
-	"sha_xor.dat" using 1:2 title "SHA-1 XOR", \
 	"crc.dat" using 1:2 title "CRC32", \
 	"adler.dat" using 1:2 title "adler32", \
 	"murmur3.dat" using 1:2 title "murmur3"
 
+set nologscale x
+set output "hash_distribution.png"
+set xrange [0:*]
+set yrange [0:*]
+set xlabel "bucket"
+set ylabel "number of results"
+set style data steps
+
+plot "sha-dist.dat" using 1:2 title "SHA-1", \
+	"crc-dist.dat" using 1:2 title "CRC32"
